19bea0e611d2d4d802d255ffe59113e96e4e30d8
dani.lin <dani.lin@webcomm.com.tw>
2024-03-06 22:29:53 +0800

    第二堂課

---
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @Qualifier("canonPrinter")
+    @Autowired
+    private Printer printer;
+
+    @RequestMapping("/test")
+    public String test() {
+        printer.print("hello world");
+        return "Hello World";
+    }
+}
