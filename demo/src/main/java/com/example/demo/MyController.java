df0d10781f154e18c7d8059dc7581161ada07968
dani.lin <dani.lin@webcomm.com.tw>
2024-03-08 23:20:01 +0800

    Json比較

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,20 +1,21 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-    @Qualifier("hpPrinter")
-    @Autowired
-    private Printer printer;
-
-    @RequestMapping("/test")
-    public String test() {
-        printer.print("hello world");
-        return "Hello World";
+    @RequestMapping("/product")
+    public String product() {
+        return  "蘋果、橘子";
     }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "名子";
+    }
+
+    
+
 }
