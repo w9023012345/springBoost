91c66b8a798f50b6976954dec4d80c3a9da396c0
dani.lin <dani.lin@webcomm.com.tw>
2024-03-09 15:21:26 +0800

    requestHeader/GET方法

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,31 +1,23 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 public class MyController {
 
-    @RequestMapping("/product")
-    public Prouduct product() {
-        Prouduct prouduct = new Prouduct();
-        List<String> furite = new ArrayList<String>();
-        furite.add("蘋果");
-        furite.add("橘子");
-        prouduct.setProduct(furite);
-        return  prouduct;
+    @RequestMapping("/test1")
+    public String test1(@RequestParam Integer id,
+                        @RequestParam(defaultValue = "amy", name = "testname") String name) {
+        System.out.println("id:" + id);
+        System.out.println("name:" + name);
+        return "hello";
     }
 
-    @RequestMapping("/user")
-    public Student user() {
-        Student student = new Student();
-        student.setName("judy");
-        return student;
-    }
-
-    
 
 }
