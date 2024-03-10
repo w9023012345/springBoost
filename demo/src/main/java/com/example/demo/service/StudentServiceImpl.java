6b38bd5e3bf79188acc69bff72d837a6e6396421
dani.lin <dani.lin@webcomm.com.tw>
2024-03-10 16:47:23 +0800

    使用@service @Repository 創立bean

--- demo/src/main/java/com/example/demo/service/StudentServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -4,10 +4,11 @@ import com.example.demo.model.Student;
 import com.example.demo.dao.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class StudentServiceImpl implements StudentService{
 
     @Autowired
