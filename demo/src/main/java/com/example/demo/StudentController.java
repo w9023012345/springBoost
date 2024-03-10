2a04948dd4849e099ba817918d891c558d81ba95
dani.lin <dani.lin@webcomm.com.tw>
2024-03-10 14:39:21 +0800

    keyholder bathupdate 自動增加的KEY 跟批次的使用

--- demo/src/main/java/com/example/demo/StudentController.java
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,16 +19,37 @@ public class StudentController {
 
     @PostMapping("/students")
     public String insert(@RequestBody Student student) {
-        String sql = "INSERT INTO student (id, name) VALUE (:studentId, :studentName)";
+        String sql = "INSERT INTO student (name) VALUE (:studentName)";
 
         Map<String, Object> map = new HashMap<>();
-        map.put("studentId", student.getId());
         map.put("studentName", student.getName());
 
-        namedParameterJdbcTemplate.update(sql, map);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
+
+        int id = keyHolder.getKey().intValue();
+        System.out.println("mysql 自動生成 id = " + id);
 
         return "執行insert sql ";
     }
+
+    @PostMapping("/students/batch")
+    public String inserList(@RequestBody List<Student> studentList) {
+        String sql = "INSERT INTO student (name) VALUE (:studentName)";
+
+        MapSqlParameterSource[] parametersSources = new MapSqlParameterSource[studentList.size()];
+
+        for (int i = 0; i < studentList.size(); i++) {
+            Student student = studentList.get(i);
+            parametersSources[i] = new MapSqlParameterSource();
+            parametersSources[i].addValue("studentName", student.getName());
+        }
+
+        namedParameterJdbcTemplate.batchUpdate(sql, parametersSources);
+        return "執行一批 insert sql ";
+    }
+
 
     @DeleteMapping("/students/{studentId}")
     public String delete(@PathVariable Integer studentId) {
