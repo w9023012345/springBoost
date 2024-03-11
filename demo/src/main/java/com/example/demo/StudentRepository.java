c6ba6e35e3e0980de302602bde2f84df142c2208
dani.lin <dani.lin@webcomm.com.tw>
2024-03-11 21:36:39 +0800

    自定義查詢條件 findByXXX

--- demo/src/main/java/com/example/demo/StudentRepository.java
+++ demo/src/main/java/com/example/demo/StudentRepository.java
@@ -2,5 +2,11 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends CrudRepository<Student, Integer>{
+
+    List<Student> findByName(String name);
+
+    Student findByIdAndName(Integer id, String name);
 }
