b25b0650c454f9e8668b8b44cc2838f70c1e1207
dani.lin <dani.lin@webcomm.com.tw>
2024-03-11 21:40:28 +0800

    使用@Query 在複雜狀況使用sql語法

--- demo/src/main/java/com/example/demo/StudentRepository.java
+++ demo/src/main/java/com/example/demo/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface StudentRepository extends CrudRepository<Student, Integer>{
     List<Student> findByName(String name);
 
     Student findByIdAndName(Integer id, String name);
+
+    @Query(value = "SELECT id,name from student WHERE id = ?1 AND name = ?2", nativeQuery = true)
+    Student test1(Integer id, String name);
 }
