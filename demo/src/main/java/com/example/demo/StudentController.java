cb1dece390ed6a315fe0d5a6f0feca4b7b36523e
dani.lin <dani.lin@webcomm.com.tw>
2024-03-11 21:22:32 +0800

    jpa crud 防呆save方法卻保有直在新增資料置資料庫

--- demo/src/main/java/com/example/demo/StudentController.java
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -16,5 +14,35 @@ public class StudentController {
         studentRepository.save(student);
 
         return  "執行creat操作";
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String update(@RequestBody Student student,
+                         @PathVariable Integer studentId){
+
+        Student s = studentRepository.findById(studentId).orElse(null);
+
+        if(s != null){
+            s.setName(student.getName());
+            studentRepository.save(s);
+            return  "執行update操作";
+        }
+
+
+        return  "update操作失敗";
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Integer studentId){
+
+        studentRepository.deleteById(studentId);
+
+        return  "執行delete操作";
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student read(@PathVariable Integer studentId){
+
+        return studentRepository.findById(studentId).orElse(null);
     }
 }
