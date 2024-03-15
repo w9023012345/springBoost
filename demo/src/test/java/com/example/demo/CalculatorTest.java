0dd8b10a2b3112c26a71517bd18b58c0d9121720
dani.lin <dani.lin@webcomm.com.tw>
2024-03-15 23:10:28 +0800

    各種常用單元測試方法  assertNotNull(result);        assertEquals(7 , result);         assertTrue(result > 1);        assertThrows(ArithmeticException.class, () -> {

--- demo/src/test/java/com/example/demo/CalculatorTest.java
+++ demo/src/test/java/com/example/demo/CalculatorTest.java
@@ -7,10 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     @Test
-    public void test() {
+    public void add() {
         Calculator calculator = new Calculator();
         int result = calculator.add(3, 4);
-        assertEquals(7, result);
+
+        assertNotNull(result);
+        assertEquals(7 , result);
+        assertTrue(result > 1);
+    }
+
+    @Test
+    public void divide(){
+        Calculator calculator = new Calculator();
+
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(10, 0);
+        });
     }
 
 }
